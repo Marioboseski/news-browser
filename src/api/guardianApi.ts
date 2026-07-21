d80597a7e528e38d71c6a574d1aad94ddac4b8f3
Mario Boseski <crboseskimario@gmail.com>
2026-07-21 19:21:26 +0200

    Implement infinite scroll

--- src/api/guardianApi.ts
+++ src/api/guardianApi.ts
@@ -1,8 +1,8 @@
 const url = `api/search`;
  const API_KEY = import.meta.env.VITE_API_KEY;
 
-export const guardianApi = async (section: string) => {
-  const res = await fetch(`${url}?section=${section}&show-fields=thumbnail,trailText,byline&api-key=${API_KEY}`);
+export const guardianApi = async (section: string, page: number) => {
+  const res = await fetch(`${url}?section=${section}&show-fields=thumbnail,trailText,byline&page=${page}&api-key=${API_KEY}`);
   if(!res.ok) {
     throw new Error("Failed to fetch news");
   }
--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { guardianApi } from "../api/guardianApi";
 import NewsCard from "../components/NewsCard";
 import type { News } from "../types/types";
@@ -7,18 +7,61 @@ const NewsPage = () => {
 
   const [section, setSection] = useState("technology");
   const [news, setNews] = useState<News[]>([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const loaderRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const fetchNews = async () => {
-      const data = await guardianApi(section);
-      setNews(data.response.results);
+      setIsLoading(true);
+
+      try {
+        const data = await guardianApi(section, page);
+
+        setNews((prevNews) => {
+          return [...prevNews, ...data.response.results];
+        });
+
+      } catch (error) {
+        console.error("Failed to fetch news:", error);
+
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchNews();
-  }, [section]);
+  }, [section, page]);
+
+  useEffect(() => {
+
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+
+      if (!entry.isIntersecting) return;
+
+      if(isLoading) return;
+
+      setPage((prevPage) => prevPage + 1);
+    });
+
+    if (!loaderRef.current) return;
+    observer.observe(loaderRef.current);
+
+    return () => {
+      observer.disconnect();
+    }
+
+  }, [isLoading]);
+
+  const handleSectionChange = (section: string) => {
+    setNews([]);
+    setPage(1);
+    setSection(section);
+  }
 
   return (
     <div>
-      <select onChange={(e) => setSection(e.target.value)}>
+      <select onChange={(e) => handleSectionChange(e.target.value)}>
         <option value="technology">Techology</option>
         <option value="sport">Sport</option>
         <option value="science">Sceince</option>
@@ -29,6 +72,8 @@ const NewsPage = () => {
         {news.map((newsItem) => (
           <NewsCard key={newsItem.id} news={newsItem} />
         ))}
+      </div>
+      <div ref={loaderRef}>
       </div>
     </div>
   );
