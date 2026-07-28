87ced013aea6510e82135332b34a42383d25a41b
Mario Boseski <crboseskimario@gmail.com>
2026-07-28 14:37:24 +0200

    Add date in NewsCard.tsx

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -2,12 +2,16 @@ import type { NewsCardProps } from "../types/types";
 import { Bookmark } from "lucide-react";
 
 const NewsCard = ({ news, onBookmark, isBookmarked, onClick }: NewsCardProps) => {
+
+  const WebDate = new Date(news.webPublicationDate).toLocaleDateString();
+
   return (
     <div onClick={() => onClick(news)} className="flex flex-col justify-center items-center text-center gap-2 p-3 border-2 border-gray-400 rounded-md w-full cursor-pointer md:max-w-sm">
       <img src={news.fields.thumbnail} alt={news.webTitle} className="max-h-40 max-w-40" />
       <p>{news.fields.trailText}</p>
       <p className="font-semibold">{news.fields.byline}</p>
-      <Bookmark onClick={(e) => {e.stopPropagation(); onBookmark(news)}} fill={isBookmarked ? "black" : "white"} className="cursor-pointer" />
+      <p>{WebDate}</p>
+      <Bookmark onClick={(e) => { e.stopPropagation(); onBookmark(news) }} fill={isBookmarked ? "black" : "white"} className="cursor-pointer" />
     </div>
   );
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,8 @@
 export type News = {
   id: string,
   webTitle: string,
-  webUrl: string
+  webUrl: string,
+  webPublicationDate: string,
 
   fields: {
     thumbnail: string,
